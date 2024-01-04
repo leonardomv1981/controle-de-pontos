bd079f21944267d44a54025c7e81f8bb54f8011e
leonardomv1981 <leonardo@leonardovilela.com>
2024-01-04 15:39:18 -0300

    front: inclusão de sidemenu em arquivo separado, rota de produto-milha

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutosmilhasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::prefix('produtos-milhas')->group(function () {
+    Route::get('/', [ProdutosmilhasController::class, 'index'])->name('produto-milhas.index');
 });
